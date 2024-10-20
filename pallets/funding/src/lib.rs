32cd5a0a4efe362f7ab1d2c0dcd1b56a8cb02f9e
milosdjurica <milosdjurica.work@gmail.com>
2024-10-20 21:08:06 +0200

    transfer_ownership function

--- pallets/funding/src/lib.rs
+++ pallets/funding/src/lib.rs
@@ -1,4 +1,3 @@
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
@@ -9,6 +8,7 @@ pub mod pallet {
     use frame_system::pallet_prelude::*;
 
     #[pallet::pallet]
+    // #[pallet::without_storage_info]
     pub struct Pallet<T>(_);
 
     #[pallet::config]
@@ -16,12 +16,46 @@ pub mod pallet {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
     }
 
-    #[pallet::event] // <-- Step 3. code block will replace this.
+    #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         // ! Create events here
+        OwnershipTransferred(T::AccountId, T::AccountId),
     }
-    //  #[pallet::error]   // <-- Step 4. code block will replace this.
-    //  #[pallet::storage] // <-- Step 5. code block will replace this.
-    //  #[pallet::call]    // <-- Step 6. code block will replace this.
+
+    // ALICE ADDR ->  5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
+    #[pallet::storage]
+    #[pallet::getter(fn owner)]
+
+    pub type Owner<T: Config> = StorageValue<_, T::AccountId, OptionQuery>;
+
+    #[pallet::error]
+    pub enum Error<T> {
+        NotOwner,
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        // Implement your pallet's callable functions here
+
+        #[pallet::weight(Weight::default())]
+        #[pallet::call_index(0)]
+        pub fn transfer_ownership(origin: OriginFor<T>, new_owner: T::AccountId) -> DispatchResult {
+            let sender = ensure_signed(origin)?;
+
+            // Ensure that the caller is the current owner
+            ensure!(
+                Some(sender.clone()) == Owner::<T>::get(),
+                Error::<T>::NotOwner
+            );
+
+            // Set new owner
+            Owner::<T>::put(&new_owner);
+
+            // Emit event
+            Self::deposit_event(Event::OwnershipTransferred(sender, new_owner));
+
+            Ok(())
+        }
+    }
 }
